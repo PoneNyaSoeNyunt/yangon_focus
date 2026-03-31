60866e203ab2719b4034e548d7ddcbc6518f1e26
PoneNyaSoeNyunt <ponenyasn@gmail.com>
2026-04-01 02:08:35 +0630

    refactor: remove hardcoded test owner and guest accounts from UserSeeder; keep only Super Admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -40,63 +39,6 @@ class UserSeeder extends Seeder
             'created_at'      => now(),
             'updated_at'      => now(),
         ];
-
-        $usersToInsert[] = [
-            'phone_number'   => '092345610',
-            'full_name'      => 'Daw Hla',
-            'nrc_number'     => '12(N)222222',
-            'password_hash'  => Hash::make('$Owner123'),
-            'role'           => 'Owner',
-            'user_status_id' => $activeStatusId,
-            'created_at'     => now(),
-            'updated_at'     => now(),
-        ];
-        $rateLimitsToInsert[] = [
-            'phone_number'    => '092345610',
-            'failed_attempts' => 0,
-            'lockout_level'   => 0,
-            'unlock_at'       => null,
-            'last_attempt_at' => null,
-            'created_at'      => now(),
-            'updated_at'      => now(),
-        ];
-
-        $faker = Faker::create();
-        $usedPhones = ['09765432189', '092345610'];
-        $usedNrcs = ['12(N)111111', '12(N)222222'];
-
-        for ($i = 0; $i < 10; $i++) {
-            do {
-                $phone = '09' . $faker->numerify('#########');
-            } while (in_array($phone, $usedPhones));
-            $usedPhones[] = $phone;
-
-            do {
-                $nrc = $faker->numerify('##/######(N)######');
-            } while (in_array($nrc, $usedNrcs));
-            $usedNrcs[] = $nrc;
-
-            $usersToInsert[] = [
-                'phone_number'   => $phone,
-                'full_name'      => $faker->name(),
-                'nrc_number'     => $nrc,
-                'password_hash'  => Hash::make('$Seeker123'),
-                'role'           => 'Guest',
-                'user_status_id' => $activeStatusId,
-                'created_at'     => now(),
-                'updated_at'     => now(),
-            ];
-
-            $rateLimitsToInsert[] = [
-                'phone_number'    => $phone,
-                'failed_attempts' => 0,
-                'lockout_level'   => 0,
-                'unlock_at'       => null,
-                'last_attempt_at' => null,
-                'created_at'      => now(),
-                'updated_at'      => now(),
-            ];
-        }
 
         DB::table('users')->insert($usersToInsert);
         DB::table('auth_rate_limits')->insert($rateLimitsToInsert);
